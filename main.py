b23c75c8bf4a7721b5b8c2de515d01f39eed267b
Yang Lu <xinyanglu664@gmail.com>
2020-04-22 19:45:06 -0400

    Added trigger words

--- main.py
+++ main.py
@@ -14,22 +14,31 @@ def interpret_text(t):
     if t == "never mind":
         pass
 
+    elif t == "turn on desk light":
+        # code to turn on desk light
+        pass
+
+
+def get_text():
+    while True:
+        try:
+            with sr.Microphone() as source:
+                r.adjust_for_ambient_noise(source, duration=0.2)
+                audio = r.listen(source)
+                text = r.recognize_google(audio, key=None, language='en-US')
+
+        except sr.RequestError as e:
+            text = "Could not request results; {0}".format(e)
+
+        except sr.UnknownValueError:
+            text = "unknown error occurred"
+
+        return text
+
 
 while True:
-    try:
-        with sr.Microphone() as source:
-            print('test')
-            r.adjust_for_ambient_noise(source, duration=0.2)
-            audio = r.listen(source)
+    if get_text() == "hey assistant":
+        user_text = get_text()
+        interpret_text(user_text)
 
-            text = r.recognize_google(audio, key=None, language='en-US')
 
-            interpret_text(text)
-
-            print(text)
-
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
-
-    except sr.UnknownValueError:
-        print("unknown error occurred")
