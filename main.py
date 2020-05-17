fc091eae14a7611128e12be91c774efb765d8fc4
Yang Lu <xinyanglu664@gmail.com>
2020-05-17 12:57:33 -0400

    Added LEDs and relay board to the circuit

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import speech_recognition as sr
 import pyttsx3
+import RPi.GPIO as GPIO
 
 r = sr.Recognizer()
+GPIO.setmode(GPIO.BCM)
 
 RED = 12
 GREEN = 16
+LAMP = 26
 
-def speak_text(command):
-    engine = pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
-
+GPIO.setup(RED, GPIO.OUT)
+GPIO.setup(GREEN, GPIO.OUT)
+GPIO.setup(LAMP, GPIO.OUT)
 
 def interpret_text(command):
     if command == "never mind":
         pass
 
     elif command == "turn on desk light":
-        # code to turn on desk light
+        GPIO.output(RED, GPIO.HIGH)
+        print("turning on desk light")
+
+    elif command == "turn off desk light":
+        GPIO.output(RED, GPIO.HIGH)
+        print("turning off desk light")
+
+    else:
         pass
 
 
@@ -38,13 +46,14 @@ def get_text(s):
 
 
 with sr.Microphone(device_index=1) as source:
-    r.adjust_for_ambient_noise(source, duration=1)
+    r.adjust_for_ambient_noise(source, duration=5)
     print(r.energy_threshold)
     r.dynamic_energy_threshold = False
 
     while True:
         t = get_text(source).lower()
+        print(t)
         if t == "hey assistant":
-
+            GPIO.output(GREEN, GPIO.HIGH)
             user_text = get_text(source).lower()
             interpret_text(user_text)
