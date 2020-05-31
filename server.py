e25130bb1951f4f2d579927380fece50863314ab
Yang Lu <xinyanglu664@gmail.com>
2020-05-31 18:06:33 -0400

    Connected GPIO code with server code

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from main import interpret_text
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
@@ -10,5 +10,6 @@ def my_form():
 
 @app.route('/getValueFromTextbox', methods=['POST'])
 def get_value():
-    value = request.body['text']
+    value = request.form['text']
     interpret_text(value)
+    return render_template('textbox.html')
