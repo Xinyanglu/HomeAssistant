b786071306f08f8a0e6cc23bcecf4f8d924c4001
Yang Lu <xinyanglu664@gmail.com>
2020-07-26 22:55:25 -0400

    Switched out textbox for a toggle button to control light

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def my_form():
-    return render_template('textbox.html')
+    return render_template('Controls.html')
 
 
 @app.route('/getValueFromTextbox', methods=['POST'])
 def get_value():
     value = request.form['text']
     interpret_text(value)
-    return render_template('textbox.html')
+    return render_template('Controls.html')
